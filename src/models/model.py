79c142f840e5787cd569de4510adfc121deae53f
Carolina <clopez@mddevelopmentcenter.com>
2023-03-21 16:34:07 +0100

    initial starting data

---
+++ src/models/model.py
@@ -0,0 +1,39 @@
+import numpy as np
+from tqdm.notebook import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.convolutional = nn.Sequential (
+            # two cnn with 8 features
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(8, 8, kernel_size=3, padding=1),
+            nn.ReLU(),
+            # max pooling 2x2
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # two cnn with 16 features
+            nn.Conv2d(8, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+
+        self.fully_connected = nn.Sequential(
+                nn.Linear(14*14*16, 500),
+                nn.ReLU(),
+                nn.Linear(500, 10),
+                nn.Softmax(dim=1))
+    
+    def forward(self, x):
+        x = self.convolutional(x)
+        #reshape x so it becomes flat, except for the first dimension (which is the minibatch)
+        x = x.view(x.size(0), -1)
+        x = self.fully_connected(x)
+        return x
