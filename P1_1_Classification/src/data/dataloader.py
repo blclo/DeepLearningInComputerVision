714f21b70662f50d4fac3f79b185bd35cef40bf3
Carolina <clopez@mddevelopmentcenter.com>
2023-03-22 15:11:05 +0100

    Classification hotdog_no_hotdog wip

--- P1_1_Classification/src/data/dataloader.py
+++ P1_1_Classification/src/data/dataloader.py
@@ -1,6 +1,6 @@
+import torch
 from typing import Optional
 from pathlib import Path
-import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import numpy as np
@@ -10,13 +10,25 @@ import os
 import glob
 import PIL.Image as Image
 
-
-class Hotdog_NotHotdog(torch.utils.data.Dataset):
-    def __init__(self, train, transform=None, size=128, data_path='/dtu/datasets1/02514/hotdog_nothotdog'):
-        'Initialization'
+class HotDog_NotHotdog(Dataset):
+    def __init__(self,
+        train,
+        transform=None,
+        size=128,
+        data_path: str = 'data/hotdog-not-hotdog',
+    ):
+        # initialization
         self.transform = transform
         self.train = train
+
         data_path = os.path.join(data_path, 'train' if train else 'test')
+        # Create as Path-object
+        #self.data_path = Path(data_path)
+
+        # Load processed data-file 
+        #datafile = torch.load(self.data_path)
+
+        # Extract data
         image_classes = [os.path.split(d)[1] for d in glob.glob(data_path +'/*') if os.path.isdir(d)]
         image_classes.sort()
         self.name_to_label = {c: id for id, c in enumerate(image_classes)}
@@ -31,7 +43,7 @@ class Hotdog_NotHotdog(torch.utils.data.Dataset):
     def __len__(self):
         'Returns the total number of samples'
         return len(self.image_paths)
-
+    
     def __getitem__(self, idx):
         'Generates one sample of data'
         image_path = self.image_paths[idx]
@@ -40,6 +52,7 @@ class Hotdog_NotHotdog(torch.utils.data.Dataset):
         X = self.convert_tensor(Image.open(image_path))
         
         # If data is for training, perform mixup, only perform mixup roughly on 1 for every 5 images
+        '''
         if self.train and idx > 0 and idx%5 == 0:
             # Choose another image/label randomly
             mixup_idx = np.random.randint(0, len(self.image_paths)-1)
@@ -54,9 +67,9 @@ class Hotdog_NotHotdog(torch.utils.data.Dataset):
             lam = np.random.beta(alpha, alpha)
             X = lam * X + (1 - lam) * mixup_image
             y = lam * y + (1 - lam) * mixup_label
+        '''
             
         if self.transform:
             X = self.transform(X)
             
         return X, y
-    
--- P1_1_Classification/src/models/train_model.py
+++ P1_1_Classification/src/models/train_model.py
@@ -13,208 +13,15 @@ import torchvision.datasets as datasets
 
 import matplotlib.pyplot as plt
 from src.data.dataloader import *
-from src.models.model import Network
-
-#  ---------------  Training  ---------------
-def train(
-        datafolder_path: str, datafile_name: str = 'catalan_dataset.pth',
-        model_name: str = 'FullyConnected',
-        batch_size: int = 128, 
-        num_workers: int = 1, 
-        test_proportion: float = 0.2, 
-        val_proportion: float = 0.2, 
-        split_type: str = 'random',
-        lr=1e-3, epochs: int = 100, loss_type: str = 'BCE', optimizer: str = 'SGD', momentum: float = 0.9,
-        experiment_name: str = str(int(round(time.time()))), save_path: str = '', 
-        seed: int = 42,
-    ):
-
-    """
-    Trains the model.
-
-    Args:
-        datafolder_path (str): _description_
-        batch_size (int, optional): _description_. Defaults to 128.
-        num_workers (int, optional): _description_. Defaults to 1.
-        test_proportion (float, optional): _description_. Defaults to 0.2.
-        val_proportion (float, optional): _description_. Defaults to 0.2.
-        split_type (str, optional): _description_. Defaults to 'random'.
-        lr (_type_, optional): _description_. Defaults to 1e-3.
-        epochs (int, optional): _description_. Defaults to 100.
-        experiment_name (str, optional): _description_. Defaults to str(int(round(time.time()))).
-    """
-    # Set seed
-    set_seed(seed)
-    current_best_loss = torch.inf
-
-    # Load dataset
-    dataset = CatalanJuvenileJustice(
-        data_path=f"{datafolder_path}/processed/{datafile_name}"
-    )
-
-    columns = dataset.getColumns()
-
-    # Split into training and test
-    train_loader, val_loader, _ = dataset.get_loaders(
-        batch_size=batch_size, 
-        shuffle=True, 
-        num_workers=1, 
-        test_size=test_proportion, 
-        val_size=val_proportion, 
-        split_type=split_type,
-    )
-
-    # Use gpu if available
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    # Standardization constants
-    mu      = train_loader.dataset.dataset.data.mean(axis=0).to(device)
-    sigma   = train_loader.dataset.dataset.data.std(axis=0).to(device)
-
-    # Define the model, loss criterion and optimizer
-    model = get_model(model_name, dataset.n_attributes).to(device)
-    criterion = get_loss_function(type=loss_type)
-    optimizer = get_optimizer(model, type=optimizer, lr=lr, momentum=momentum)
-
-    print("MLP Architecture:")
-    print(model)
-
-    fairness = Fairness_criteria(columns)
-    Independence_dict = {}
-    #print('Sensitive extended: ', sensitive_extended)
-    #for i in sensitive_extended:
-    #    Independence_dict[i] = 0
-    writer = SummaryWriter(f"logs/{experiment_name}")
-    with trange(epochs) as t:
-        for epoch in t:
-            running_loss_train, running_loss_val    = 0.0, 0.0
-            running_acc_train,  running_acc_val     = 0.0, 0.0
-
-            for batch in iter(train_loader):
-                # Extract data                
-                inputs, labels = batch['data'].to(device), batch['label'].to(device)
-
-                # Zero the parameter gradients
-                optimizer.zero_grad()
-
-                # Standardize inputs
-                inputs = (inputs - mu) / sigma
-                # Forward + backward
-                outputs = model(inputs)
-                y_pred = outputs['pred']
-
-                loss = criterion(y_pred.float(), labels.float())
-                running_loss_train += loss.item()
-                loss.backward()
-
-                # Optimize
-                optimizer.step()
-                
-                # Store accuracy
-                equals = (y_pred >= 0.5) == labels.view(*y_pred.shape)
-                running_acc_train += torch.mean(equals.type(torch.FloatTensor))
-
-            # Validation
-            with torch.no_grad():
-                for batch in iter(val_loader):
-                    inputs_before, labels = batch['data'].to(device), batch['label'].to(device)
-                    #print('input shape: ', inputs.shape)
-
-                    # Standardize inputs
-                    inputs = (inputs_before - mu) / sigma
-                    
-                    # Get predictions
-                    outputs = model(inputs)
-                    y_pred = outputs['pred']
-
-                    # Compute loss and accuracy
-                    running_loss_val += criterion(y_pred.float(), labels.float())
-                    equals = (y_pred >= 0.5) == labels.view(*y_pred.shape)
-                    running_acc_val += torch.mean(equals.type(torch.FloatTensor))
-
-                    tmp_independence = fairness.Independence(y_pred, inputs_before)
-                    Independence_dict_tmp = {k: (tmp_independence.get(k, 0) + Independence_dict.get(k, 0)) for k in set(tmp_independence) | set(Independence_dict)}
-                    Independence_dict = Independence_dict_tmp
-                    #print(Independence_dict)
-
-            if running_loss_val / len(val_loader) < current_best_loss:
-                current_best_loss = running_loss_val / len(val_loader)
-                # Create and save checkpoint
-                checkpoint = {
-                    "experiment_name": experiment_name,
-                    "seed": seed,
-                    "model": {
-                        'name': model_name,
-                        'net': model.net,
-                        'input_parameters': model.input_params,
-                    },
-                    "input_parameters": {
-                        "input_size": dataset.n_attributes,
-                        "output_size": 1,
-                    },
-                    "training_parameters": {
-                        "save_path": save_path,
-                        "lr": lr,
-                        "epochs": epochs,
-                        "batch_size": batch_size,
-                        "device": device,
-                        "loss_type": loss_type,
-                        "optimizer": {
-                            "name": optimizer,
-                            "momentum": momentum,
-                        },
-                    },
-                    "data": {
-                        "filename": datafile_name,
-                        "standardization": {
-                            "mu": mu,
-                            "sigma": sigma,
-                        },
-                        "split": {
-                            "test_proportion": test_proportion,
-                            "val_proportion": val_proportion,
-                            "split_type": split_type,
-                        },
-                    },
-                    "best_epoch": epoch + 1,
-                    "state_dict": model.state_dict(),
-                }
-                os.makedirs(f"{save_path}/{experiment_name}", exist_ok=True)
-                torch.save(checkpoint, f"{save_path}/{experiment_name}/best.ckpt")
-
-
-            # Update progress bar
-            train_loss_descr = (
-                f"Train loss: {running_loss_train / len(train_loader):.3f}"
-            )
-            val_loss_descr = (
-                f"Validation loss: {running_loss_val / len(val_loader):.3f}"
-            )
-            train_acc_descr = (
-                f"Train accuracy: {running_acc_train / len(train_loader):.3f}"
-            )
-            val_acc_descr = (
-                f"Validation accuracy: {running_acc_val / len(val_loader):.3f}"
-            )
-            t.set_description(
-                f"EPOCH [{epoch + 1}/{epochs}] --> {train_loss_descr} | {val_loss_descr} | {train_acc_descr} | {val_acc_descr} | Progress: "
-            )
-
-            writer.add_scalar(f'{loss_type}/train',         running_loss_train  / len(train_loader),    epoch)
-            writer.add_scalar('accuracy/train',             running_acc_train   / len(train_loader),    epoch)
-            writer.add_scalar(f'{loss_type}/validation',    running_loss_val    / len(val_loader),      epoch)
-            writer.add_scalar('accuracy/validation',        running_acc_val     / len(val_loader),      epoch)
-
-        print('\n-- Independence criteria --')
-        for key, value in Independence_dict.items():
-            print(key, 'has the \"acceptance\" rate of: ', value/(epochs*len(iter(val_loader))))
-
+from src.data.dataloader import HotDog_NotHotdog
+from src.models.model import Network     
 
 if __name__ == '__main__':
+    batch_size = 64
     # -----------------  Load dataset  -----------------
-    trainset = Hotdog_NotHotdog(train=True)
+    trainset = HotDog_NotHotdog(train=True)
     train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
-    testset = Hotdog_NotHotdog(train=False)
+    testset = HotDog_NotHotdog(train=False)
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
     
     # -----------------  Split Dataset  -----------------
@@ -251,26 +58,60 @@ if __name__ == '__main__':
     #Initialize the optimizer
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
-    # TODO: Define the loss function
-
     # TODO: Print model architecture
+    print(model)   
     
     # TODO: Notebook analysing the images
 
     # -----------------  Train model  -----------------
     # TODO: Define train function with basics
-    train(
-        datafolder_path = 'data',
-        model_name='AutoEncoder',
-        datafile_name='catalan_dataset_without_sensitives.pth',
-        batch_size = 64, 
-        epochs = 200, 
-        lr=1e-4,
-        test_proportion=0.4,
-        loss_type='BCE',
-        optimizer='Adam',
-        experiment_name=f'AutoEncoder-0.4test_prop-no_sensitive_data.lr=1e-4',
-        save_path='models',
-    )
+    num_epochs = 5
 
-    # -----------------  Test model  -----------------
+    for epoch in tqdm(range(num_epochs), unit='epoch'):
+        #For each epoch
+        train_correct = 0
+        running_loss_train, running_loss_test = 0.0, 0.0
+        test_loss = 0
+
+        for minibatch_no, (data, target) in tqdm(enumerate(train_loader), total=len(train_loader)):
+            # Move the data to the device
+            data, target = data.to(device), target.to(device)
+            # Zero the gradients computed for each weight
+            optimizer.zero_grad()
+            # Forward pass your image through the network
+            output = model(data)
+            # Compute the loss
+            loss = F.nll_loss(torch.log(output), target)
+            # Training loss
+            running_loss_train += loss
+            # Backward pass through the network
+            loss.backward()
+            # Update the weights
+            optimizer.step()
+        
+            # Compute how many were correctly classified
+            predicted = output.argmax(1)
+            train_correct += (target==predicted).sum().cpu().item()
+
+        # -----------------  Test model  -----------------
+        # Compute the test accuracy
+        test_correct = 0
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            with torch.no_grad():
+                output = model(data)
+                # Compute batch loss
+                test_loss += F.nll_loss(output, target).item()
+            # Get predicted class
+            predicted = output.argmax(1).cpu()
+            # Count correct predictions
+            target = target.cpu()
+            test_correct += (target==predicted).sum().item()
+
+        train_acc = train_correct/len(trainset)
+        test_acc = test_correct/len(testset)
+        print(f"EPOCH [{epoch + 1}/{num_epochs}]: ")
+        print("Accuracy train: {train:.1f}%\t test: {test:.1f}%".format(test=100*test_acc, train=100*train_acc))
+        print(f"Train loss: {running_loss_train / len(train_loader):.3f}")
+        print(f"Test loss: {test_loss / len(test_loader):.3f}")
+
