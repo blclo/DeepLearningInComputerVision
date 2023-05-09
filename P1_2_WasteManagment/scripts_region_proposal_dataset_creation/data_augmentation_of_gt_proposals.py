3a29e06ea9b2666fa947a82ae4a1d5815f8ab452
blclo <blclo@github.com>
2023-05-09 11:08:46 +0200

    Added the posibility of augmenting the validation dataset

--- P1_2_WasteManagment/scripts_region_proposal_dataset_creation/data_augmentation_of_gt_proposals.py
+++ P1_2_WasteManagment/scripts_region_proposal_dataset_creation/data_augmentation_of_gt_proposals.py
@@ -10,7 +10,18 @@ import numpy as np
 import torchvision.utils as utils
 
 if __name__ == '__main__':
-    main_proposals_path = "/work3/s212725/WasteProject/data/json/train_region_proposals.json"
+    type_dataset = "val"
+
+    if type_dataset == "train":
+        training_dataset_path = "/work3/s212725/WasteProject/data/json/train_region_proposals.json"
+    elif type_dataset == "val":
+        validation_dataset_path = "/work3/s212725/WasteProject/data/json/val_region_proposals.json"
+    else: 
+        print("Error: type_dataset must be train or val")
+        exit()
+
+    # define type of dataset for augmentation
+    main_proposals_path = validation_dataset_path
     # load json file in main_proposals_path
     with open(main_proposals_path, 'r') as f:
         data = json.load(f)
@@ -19,9 +30,13 @@ if __name__ == '__main__':
     transformed_gt_crops_with_labels = {}
     
     for path_to_crop, label in data.items():
+        # filename will be used to save the augmented crops
         filename = path_to_crop.split("/")[-2:] # split path by "/", get the last two elements
         filename = "/".join(filename)  # join the last two elements with "/"
-        pattern = r'^/work3/s212725/WasteProject/data/crops/batch_\d+/[0-9]+\d_gt\d+\.jpg$'
+        if type_dataset == "train":
+            pattern = r'^/work3/s212725/WasteProject/data/crops/batch_\d+/[0-9]+\d_gt\d+\.jpg$'
+        elif type_dataset == "val":
+            pattern = r'^/work3/s212725/WasteProject/data/val_proposals/crops/batch_\d+/[0-9]+\d_gt\d+\.jpg$'
         match = re.match(pattern, path_to_crop)
         if match:
             print(f"This path has match as a gt patch: {path_to_crop}")
@@ -41,5 +56,5 @@ if __name__ == '__main__':
                 transformed_gt_crops_with_labels[path_transformed_gt] = label
     
     json_object = json.dumps(transformed_gt_crops_with_labels, indent=4)
-    with open("/work3/s212725/WasteProject/data/json/gt_augmented_proposals.json", "w") as outfile:
+    with open(f"/work3/s212725/WasteProject/data/json/gt_augmented_proposals_in_{type_dataset}_dataset.json", "w") as outfile:
         outfile.write(json_object)
