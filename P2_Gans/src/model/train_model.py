45ed496317e4f7b347df13a6ef99b3a47b39dbd8
blclo <blclo@github.com>
2023-05-30 18:46:03 +0200

    svm to learn latent direction

--- P2_Gans/src/model/train_model.py
+++ P2_Gans/src/model/train_model.py
@@ -1,19 +1,10 @@
-#from src.data.dataloader import LatentDataset
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from torch.utils.data import DataLoader
 import numpy as np
 import os
 import torch
 
-'''
-train_dataset = LatentDataset(train=True)
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-
-test_dataset = LatentDataset(train=False)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
-'''
 latent_codes_dir_with_sunglasses = '/work3/s212725/GANs/data/with_sunglasses'
 latent_codes_dir_without_sunglasses = '/work3/s212725/GANs/data/without_sunglasses'
 
@@ -21,26 +12,50 @@ latent_codes_dir_without_sunglasses = '/work3/s212725/GANs/data/without_sunglass
 latent_codes = []
 labels = []
 
-for latent_code in os.listdir(latent_codes_dir_with_sunglasses):
-    latent_code = torch.load(os.path.join(latent_codes_dir_with_sunglasses, latent_code))
+for latent_code_file in os.listdir(latent_codes_dir_with_sunglasses):
+    latent_code_path = os.path.join(latent_codes_dir_with_sunglasses, latent_code_file)
+    latent_code_data = np.load(latent_code_path)
+    latent_code = latent_code_data['w']  # Assuming the key for latent codes is 'latent_code'
+    latent_code = np.squeeze(latent_code)  # Remove the extra dimension
+    latent_code = latent_code.flatten()  # Flatten the latent code
     latent_codes.append(latent_code)
-    labels.append(1) # with sunglasses
+    labels.append(1)  # with sunglasses
 
-for latent_code in os.listdir(latent_codes_dir_without_sunglasses):
-    latent_code = torch.load(os.path.join(latent_codes_dir_without_sunglasses, latent_code))
+for latent_code_file in os.listdir(latent_codes_dir_without_sunglasses):
+    latent_code_path = os.path.join(latent_codes_dir_without_sunglasses, latent_code_file)
+    latent_code_data = np.load(latent_code_path)
+    latent_code = latent_code_data['w']  # Assuming the key for latent codes is 'latent_code'
+    latent_code = np.squeeze(latent_code)  # Remove the extra dimension
+    latent_code = latent_code.flatten()  # Flatten the latent code
     latent_codes.append(latent_code)
-    labels.append(0) # without sunglasses
+    labels.append(0)  # without sunglasses
+
+# Convert the latent codes and labels to numpy arrays
+latent_codes = np.array(latent_codes)
+print("Initial latent_codes shape: ", latent_codes.shape)
+labels = np.array(labels)
 
 # Step 2: Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(latent_codes, labels, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(latent_codes, labels, test_size=0.4, random_state=42)
 
-# Step 3: Preprocess the data if needed (e.g., scaling, normalization)
+# Convert the training data to torch tensors
+X_train = torch.from_numpy(X_train).float()
+print("Initial shape: ", X_train.shape)
+y_train = torch.from_numpy(y_train)
+
+# Reshape X_train to the desired shape (1, 18, 512)
+X_train = X_train.view(X_train.shape[0], *X_train.shape[1:])
+
 
 # Step 4: Train the SVM model
 svm_classifier = svm.SVC(kernel='linear')  # Create an SVM classifier with linear kernel
 svm_classifier.fit(X_train, y_train)  # Fit the classifier to the training data
+print(X_train.shape)
+print(y_train.shape)
 
 # Step 5: Evaluate the model
+X_test = torch.from_numpy(X_test).float()
+X_test = X_test.view(X_test.shape[0], *X_test.shape[1:])
 y_pred = svm_classifier.predict(X_test)  # Predict labels for the test data
 accuracy = accuracy_score(y_test, y_pred)  # Calculate accuracy
 print("Accuracy:", accuracy)
@@ -49,8 +64,14 @@ print("Accuracy:", accuracy)
 support_vectors = svm_classifier.support_vectors_
 coefficients = svm_classifier.coef_[0]
 
-# Extract the latent direction from the coefficients
-latent_direction = np.dot(support_vectors.T, coefficients)
+print(coefficients.shape)
+# Step 6: Normalize the weight vector to obtain the latent direction
+latent_direction = coefficients / np.linalg.norm(coefficients)
 
-# Normalize the latent direction (optional)
-latent_direction /= np.linalg.norm(latent_direction)
+print(latent_direction.shape)
+# Specify the original shape to unflatten the array
+original_shape = (1, 18, 512)
+
+# Unflatten the array
+unflattened_array = latent_direction.reshape(original_shape)
+print(unflattened_array.shape)
